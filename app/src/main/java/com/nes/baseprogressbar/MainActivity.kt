0edf393748047dd106f4ab913ba13bb4a061c143
Prasit Singzom <xitlbmtbn_nes@hotmail.com.hk>
2020-02-24 22:47:03 +0700

    - images

--- app/src/main/java/com/nes/baseprogressbar/MainActivity.kt
+++ app/src/main/java/com/nes/baseprogressbar/MainActivity.kt
@@ -2,11 +2,31 @@ package com.nes.baseprogressbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var progress = 0.0F
+
+        Thread(Runnable {
+            while (progress <= progressbar.max){
+                progress += 1F
+
+                Thread.sleep(20)
+
+                runOnUiThread {
+                    progressbar.progress = progress
+                }
+
+                if(progress == progressbar.max){
+                    progress = 0.0F
+                }
+            }
+        }).start()
     }
 }
--- library/src/main/java/com/nesprasit/library/BaseProgressbar.kt
+++ library/src/main/java/com/nesprasit/library/BaseProgressbar.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.nes.library
+package com.nesprasit.library
 
 import android.annotation.SuppressLint
 import android.content.Context
